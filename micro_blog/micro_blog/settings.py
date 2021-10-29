45805692cc46798157473aa93c776fd987f48355
roman <chiraololo@gmail.com>
2021-10-30 03:58:57 +0700

    create tbase, footer, nav templates(for all) and sign up templates in users app. Also create routes, settings and models for thouse app

--- micro_blog/micro_blog/settings.py
+++ micro_blog/micro_blog/settings.py
@@ -54,10 +54,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'micro_blog.urls'
 
+TEMPLATES_DIR = BASE_DIR / 'templates'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
