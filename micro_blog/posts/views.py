2842ef9a060b684c1f9db9889c89862024636813
roman <chiraololo@gmail.com>
2021-10-30 17:34:01 +0700

    create some func for posts

--- micro_blog/posts/views.py
+++ micro_blog/posts/views.py
@@ -4,26 +4,84 @@ from django.contrib.auth.decorators import login_required
 from .models import Post, Group, User, Follow
 from .forms import PostForm, CommentForm
 
+POSTS_ON_PAGE_COUNT = 1
+
 def _get_posts(request, filter_: dict):
-    pass
+    post_list = Post.objects.filter(**filter_).prefetch_related(
+        'author',
+        'group',
+        'comments',
+    ).all()
+    paginator = Paginator(post_list, POSTS_ON_PAGE_COUNT)
+    page = paginator.get_page(request.GET.get('page'))
+    return page
 
 def _read_author(username):
-    pass
+    author = get_object_or_404(User, username=username)
+    post_count = Post.objects.filter(author=author).count()
+    follow_count = Follow.objects.filter(
+        author=author
+    ).count()
+    self_follow_count = Follow.objects.filter(
+        user=author
+    ).count()
+    return author, post_count, follow_count, self_follow_count
 
 def _read_post(request, post_id):
     pass
 
 def index(request):
-    pass
+    page = _get_posts(request, {})
+    return render(
+        request,
+        'index.html',
+        {
+            'page': page,
+            'page_number': page.number
+        }
+    )
 
 def group_posts(request, slug):
-    pass
+    group = get_object_or_404(Group, slug=slug)
+    page = _get_posts(request, {'group': group})
+    return render(
+        request,
+        'group.html',
+        {
+            'group': group,
+            'page': page,
+        }
+    )
 
 def profile(request, username):
-    pass
-
+    author, count_post, follow_count, self_follow_count = _read_author(username)
+    page = _get_posts(
+        request,
+        {'author':author}
+    )
+    check_follow = Follow.objects.filter(
+        user=request.user, author=author
+    ).exists()
+    return render(
+        request,
+        'profile.html',
+        {
+            'page': page,
+            'author': author,
+            'profile': author,
+            'count_post': count_post,
+            'following': check_follow,
+            'follow_count': follow_count,
+            'self_follow_count': self_follow_count,
+        }
+    )
 def post_view(request, username, post_id):
-    pass
+    context = _read_post(request, post_id)
+    return render(
+        request,
+        'post.html',
+        context,
+    )
 
 def add_comment(request, username, post_id):
     pass
