a8fcda308ec68a598658b99f227cbfb7c0fed669
roman <chiraololo@gmail.com>
2021-10-30 16:58:00 +0700

    create forms for posts app

--- micro_blog/posts/views.py
+++ micro_blog/posts/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
-from .models
+from .models import Post, Group, User, Follow
