065250c5205d161c0f9a60d05c24c087f7d6ed2f
roman <chiraololo@gmail.com>
2021-10-30 16:51:34 +0700

    create all models for posts app

--- micro_blog/posts/models.py
+++ micro_blog/posts/models.py
@@ -1,3 +1,75 @@
+from django.contrib import auth
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models import constraints
 
-# Create your models here.
+User = get_user_model
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField('Date published', auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+    )
+    group = models.ForeignKey(
+        'Group',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    image = models.ImageField(
+        upload_to='posts/',
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+
+    def __str__(self) -> str:
+        return self.text[:30]
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        'Post',
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    text = models.TextField()
+    created = models.DateTimeField('Date published', auto_now_add=True)
+
+class Follow(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user','author'],
+                name='unique_follow',
+            )
+        ]
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
