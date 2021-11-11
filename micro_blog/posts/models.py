a9f4f0b976974cd6806627241a7c52050deea4e5
Roman Cherkasov <palkamon@yandex.ru>
2021-11-11 21:26:01 +0700

    create Post model

--- micro_blog/posts/models.py
+++ micro_blog/posts/models.py
@@ -1,3 +1,36 @@
+from typing import Text
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.fields.files import ImageField
 
-# Create your models here.
+User = get_user_model()
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+    )
+
+    group = models.ForeignKey(
+        'Group',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    
+    image = models.ImageField(
+        upload_to='posts/',
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+
+    def __str__(self) -> str:    
+        # переопределение строкового метода для удобства тестирования
+        # выводит только первые 20 символов текста статьи
+        return self.text[:20]
