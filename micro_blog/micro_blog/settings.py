4e77aca5c2f9ac0ec561f257e7a1f302b14b4faf
Roman Cherkasov <palkamon@yandex.ru>
2021-11-12 14:11:29 +0700

    add static and media root

--- micro_blog/micro_blog/settings.py
+++ micro_blog/micro_blog/settings.py
@@ -84,6 +84,14 @@ DATABASES = {
     }
 }
 
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "static"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
+
+LOGIN_URL = "/auth/login/"
+LOGIN_REDIRECT_URL = "index"
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -121,7 +129,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
